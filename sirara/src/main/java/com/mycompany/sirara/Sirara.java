c6110da2a0aa7259cb4abc7d8b0f6af20e4148d7
sirara1 <126028573+sirara1@users.noreply.github.com>
2024-01-01 22:13:10 +0100

    Add files via upload

---
+++ sirara/src/main/java/com/mycompany/sirara/Sirara.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sirara;
+
+/**
+ *
+ * @author 21654
+ */
+public class Sirara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
